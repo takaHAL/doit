ce24c3cd73fdac3ffc6757e9fded509ff6dc9ae0
takaHAL <takaonoyuki0610@gmail.com>
2019-10-23 16:51:25 +0900

    画像サイズ変更

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,7 +1,7 @@
 window.onload = function () {
 	images = document.querySelectorAll('img')
 	images.forEach(function(element) {
-		element.setAttribute('src', chrome.extension.getURL('images/targetImage.png'))
+		element.src = chrome.extension.getURL('images/targetImage.png')
 		element.removeAttribute('srcset')
 	})
 }
